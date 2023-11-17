4fec9ac36cb06660c8fafd2875ae438f4833783e
jana <janask13699@gmail.com>
2023-11-17 19:58:24 +0530

    Add project files.

---
+++ read&readline/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace read_readline
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Woof!");
+            Console.WriteLine("The dog is barking.");
+        }
+    }
+
+    
+}
---
+++ read&readline/Program.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Using Console.Read
+//        Console.Write("Enter a character: ");
+//        int charCode = Console.Read();
+//        Console.WriteLine("You entered (as int): " + charCode);
+//        Console.WriteLine("You entered (as char): " + (char)charCode);
+
+//        // Using Console.ReadLine
+//        Console.Write("Enter a string: ");
+//        string userInput = Console.ReadLine();
+//        Console.WriteLine("You entered: " + userInput);
+//    }
+//}
+
+
+using read_readline;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //// Using Console.Read
+        //Console.Write("Enter a character: ");
+        //int charCode = Console.Read();
+        //Console.WriteLine($"You entered (as int): {charCode}");
+        //Console.WriteLine($"You entered (as char): {(char)charCode}");
+
+        //// Clear the input buffer before reading a string
+        //Console.ReadLine();
+
+        //// Using Console.ReadLine
+        //Console.Write("Enter a string: ");
+        //string userInput = Console.ReadLine();
+        //Console.WriteLine($"You entered: {userInput}");
+    
+            Dog myDog = new Dog();
+            myDog.Bark();
+            myDog.Eat();
+        
+    }
+
+}
+
